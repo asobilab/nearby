b17501d55f416a69ed7faa849f46ae63ca1e05da
coilo <coilo.dev@gmail.com>
2015-02-28 12:40:01 +0900

    Refacterd "Location#__construct"

--- lib/Location.php
+++ lib/Location.php
@@ -3,41 +3,32 @@
 namespace Asobilab\Nearby;
 
 use Exception;
+use Psr\Log\InvalidArgumentException;
 
 class Location
 {
+
     // GeodeticLatitude (測地緯度) - [一般的な「緯度」]
     protected $latitude;
     // GeodeticLongitude(測地経度)
     protected $longitude;
 
+    /**
+     * @param $lat  緯度
+     * @param $lon  経度
+     */
     public function __construct($lat, $lon)
     {
-        try {
-            // 入力値のValiadtion(共通)
-            if ($this->validateParams($lat) === false) {
-                throw new Exception('緯度に数値以外の指定がされています');
-            } else {
-                $latitude = floatval($lat);
+        $distrust = ['latitude' => $lat, 'longitude' => $lon];
+        foreach ($distrust as $key => $val) {
+            if ($this->validateParams($val, $key) === false) {
+                #[TODO:Hir0TKHS] エラーメッセージの詳細化をお任せします。
+                throw new InvalidArgumentException('');
             }
-            if ($this->validateParams($lon) === false) {
-                throw new Exception('経度に数値以外の指定がされています');
-            } else {
-                $longitude = floatval($lon);
-            }
-            // 入力値のValidation(個別)
-            if ($latitude < 0 || $latitude > 90) {
-                throw new Exception('緯度の数値の範囲が不正です');
-            }
-            if ($longitude < 0 || $longitude > 180) {
-                throw new Exception('経度の数値の範囲が不正です');
-            }
-        } catch (Exception $e) {
-            die($e->getMessage());
         }
 
-        $this->latitude = $latitude;
-        $this->longitude = $longitude;
+        $this->latitude = $lat;
+        $this->longitude = $lon;
     }
 
     public function getCoordinate()
@@ -55,18 +46,27 @@ class Location
         return $this->longitude;
     }
 
-    private function validateParams($input)
+    /**
+     * 緯度、経度の検証を行う。
+     *
+     * @param $input
+     * @param $use
+     * @return bool|mixed
+     */
+    private function validateParams($input, $use)
     {
-        // 値が数値型のみ許可
-        if (is_float($input) | is_int($input) | is_double($input)) {
-            return true;
-        } else {
-            return false;
-        }
-        if (preg_match('/^([1-9][0-9]*|0)(.[0-9]+)?$/', $input)) {
-            return true; // 正の整数か小数
-        } else {
-            return false;
-        }
+        $range = ['latitude' => ['min' => 0.0, 'max' => 90.0],
+                  'longitude' => ['min' => 0.0, 'max' => 180]];
+
+        return filter_var($input, FILTER_CALLBACK, ['options' => function($val) use (&$range, &$use) {
+            if (filter_var($val, FILTER_VALIDATE_FLOAT) === false) {
+                    return false;
+            }
+            $r = $range[$use];
+            if ($val < $r['min'] || $r['max'] < $val) {
+                return false;
+            }
+        }]);
+        return true;
     }
 }
