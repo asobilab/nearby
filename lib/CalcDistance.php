624a1eef7197a43d3e552257f92ee5231d88e10b
Hir0TKHS <ht.activities@gmail.com>
2015-03-03 14:54:22 +0900

    Local variable to static variable

--- lib/CalcDistance.php
+++ lib/CalcDistance.php
@@ -6,6 +6,11 @@ require 'Location.php';
 
 class CalcDistance
 {
+    // クラス定数
+    // WGS84準拠楕円体
+    private static $equatorialRadius = 6378137.0;  // 赤道半径
+    private static $oblateness = 0.00335281066474; // 扁平率
+    private static $polarRadius = 6356752.31425;   // 極半径 = (赤道半径 - 扁平率) / 赤道半径
 
     /**
      * ２点間の直線距離を求める（Lambert-Andoyer）
@@ -21,11 +26,6 @@ class CalcDistance
         $latB = $locB->getLatitude();
         $lonB = $locA->getLongitude();
 
-        // WGS84準拠楕円体
-        $equatorialRadius = 6378137.0;         // 赤道半径
-        $oblateness = 1 / 298.257223563; // 扁平率
-        $polarRadius = $equatorialRadius * (1.0 - $oblateness);   // 極半径  F = (赤道半径 - 扁平率) / 赤道半径
-
         // Convert Degrees To Radian
         $latA = deg2rad($latA);
         $lonA = deg2rad($lonA);
@@ -33,8 +33,8 @@ class CalcDistance
         $lonB = deg2rad($lonB);
 
         // Convert Geodetic Latitude To Parametic Latitude
-        $parameticA = atan($polarRadius/$equatorialRadius) * tan($latA);
-        $parameticB = atan($polarRadius/$equatorialRadius) * tan($latB);
+        $parameticA = atan(self::$polarRadius/self::$equatorialRadius) * tan($latA);
+        $parameticB = atan(self::$polarRadius/self::$equatorialRadius) * tan($latB);
 
         // Spherical Distance
         $sphericalDistance = acos(sin($parameticA)*sin($parameticB) + cos($parameticA)*cos($parameticB)*cos($lonA-$lonB));
@@ -44,10 +44,10 @@ class CalcDistance
         $sinSphericalDistance = sin($sphericalDistance / 2);
         $cosGroup = (sin($sphericalDistance) - $sphericalDistance) * pow(sin($parameticA) + sin($parameticB), 2) / $cosSphericalDistance / $cosSphericalDistance;
         $sinGroup = (sin($sphericalDistance) + $sphericalDistance) * pow(sin($parameticA) - sin($parameticB), 2) / $sinSphericalDistance / $sinSphericalDistance;
-        $delta = $oblateness / 8.0 * ($cosGroup - $sinGroup);
+        $delta = self::$oblateness / 8.0 * ($cosGroup - $sinGroup);
 
         // Geodetic Distance
-        $distance = $equatorialRadius * ($sphericalDistance + $delta); // $distance is meter.
+        $distance = self::$equatorialRadius * ($sphericalDistance + $delta); // $distance is meter.
 
         return $distance;
     }
