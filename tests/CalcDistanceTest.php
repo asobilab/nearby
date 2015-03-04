d95ae4ea3616f60d61296010dfa2339395a05bcc
Hir0TKHS <ht.activities@gmail.com>
2015-03-05 01:33:00 +0900

    Add CalcDistanceTest

---
+++ tests/CalcDistanceTest.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ *  @package    Asobiab\Nearby\Test
+ *  @author     Hir0TKHS <ht.activities@gmail.com>
+ *  @license    MIT License
+ *  @version    1.0.0
+ *  @link       https://github.com/asobilab/nearby
+ */
+
+namespace Asobilab\Nearby\Nearby;
+
+use Asobilab\Nearby\Nearby;
+use Asobilab\Nearby\Location;
+use Asobilab\Nearby\CalcDistance;
+
+Class CalcDistanceTest extends BaseUnit
+{
+    
+}
