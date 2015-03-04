59eecb2640b2f1012edf0b7dc59a93a40594377f
Hir0TKHS <ht.activities@gmail.com>
2015-03-05 04:08:46 +0900

    Add testcases to CalcDistanceTest

--- tests/CalcDistanceTest.php
+++ tests/CalcDistanceTest.php
@@ -18,6 +18,34 @@ Class CalcDistanceTest extends BaseUnit
 {
     public function testCalcDistance()
     {
-        
+        $locationA = new Location(35.54804,139.724778);
+        $locationB = new Location(35.554694,139.732664);
+        $result = CalcDistance::getDistance($locationA,$locationB);
+        $this->assertEquals($result,1148.2904126236);
+    }
+
+    public function testCalcDistanceFailure()
+    {
+        $locationA = new Location(35.54804,139.724778);
+        $locationB = new Location(35.554694,139.732664);
+        $result = CalcDistance::getDistance($locationA,$locationB);
+        $this->assertNotEquals($result,11111.11);
+    }
+
+
+    public function locationAndDistanceProvider()
+    {
+        return [[35.54804,139.724778,35.554694,139.732664,1148.2904126236]];
+    }
+
+    /**
+     * @dataProvider    locationAndDistanceProvider
+     */
+    public function testCalcDistanceWithArguments($latA,$lonA,$latB,$lonB,$distance)
+    {
+        $locationA = new Location($latA,$lonA);
+        $locationB = new Location($latB,$lonB);
+        $calcResult = CalcDistance::getDistance($locationA,$locationB);
+        $this->assertEquals($calcResult,$distance);
     }
 }
